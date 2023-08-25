d353c86437b954effee29f347dea17b6cca1ddf3
JessicaLais <142358374+JessicaLais@users.noreply.github.com>
2023-08-24 22:41:24 -0300

    Update Exercicio1.java

--- atividade2/Exercicio1.java
+++ atividade2/Exercicio1.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 
 public class Exercicio1{
     public static void main(String[] args){
+
+        System.out.print("==========CALCULADORA==========\n\n");
         
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite o primeiro número da operação: ");
