2b260da834eb5506cccd24f94a9f03bdb5d231e3
JessicaLais <142358374+JessicaLais@users.noreply.github.com>
2023-08-24 21:45:22 -0300

    Update Exercicio1.java

---
+++ atividade2/Exercicio1.java
@@ -0,0 +1,38 @@
+package atividade2;
+import java.util.Scanner;
+
+public class Calculadora{
+    public static void main(String[] args){
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o primeiro número da operação: ");
+        double numero1 = scanner.nextDouble();
+
+        System.out.print("Digite a OPERAÇÃO (+   -   *  /): ");
+        char operador = scanner.next().charAt(0);
+
+        System.out.print("Digite o segundo número da operação: ");
+        double numero2 = scanner.nextDouble();
+
+        Operar(numero1, operador, numero2, scanner);
+        scanner.close();
+    }
+    public static void Operar(double numero1, char operador, double numero2, Scanner scanner){
+        double resultado = 0.0;
+        switch (operador) {
+            case '+':
+                resultado = numero1 + numero2;
+                break;
+            case '-':
+                resultado = numero1 - numero2;
+                break;
+            case '*':
+                resultado = numero1 * numero2;
+                break;
+            case '/':
+                resultado = numero1 / numero2;
+                break;
+        }
+        System.out.println("Resultado: " + resultado);
+    }
+}
